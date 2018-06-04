c3c112dcb5a5b87ab3046ba318d6fca6a2d0783d
krisztiank <krisztian.korosi@caplin.com>
2018-06-04 16:17:04 +0100

    Propose os-service

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ This package brings ability to deploy webapps a lot easier. Just copy [configura
 ### Dependencies to run
 * [NodeJS](https://nodejs.org/en/)
 * [Yarn](https://yarnpkg.com/en/)
+* Windows build tools (for Windows usage)
+    1. First install [Python](https://www.python.org/)
+    1. From an administrator terminal:
+
+        ```npm install --global --production windows-build-tools```
+
 
 ### Usage
 
---
+++ utils/manageService.js
@@ -0,0 +1,36 @@
+const service = require('os-service');
+const minimist = require('minimist');
+const {resolve} = require('path');
+
+const {add, remove, run} = minimist(process.argv.slice(2));
+
+const SERVICE_NAME = 'node-webserver';
+
+if (add) {
+    service.add(SERVICE_NAME, {programPath: resolve('../server.js')}, function(error){
+        if (error) {
+            console.trace(error);
+        }
+    });
+} else if (remove) {
+    service.remove(SERVICE_NAME, function(error){
+        if (error) {
+            console.trace(error);
+        }
+    });
+} else if (run) {
+    service.run(function () {
+        service.stop (0);
+    });
+} else {
+    console.info(`
+    Usage:
+    
+    yarn run service [argument]
+    
+    arguments:
+        --add      Installs the service
+        --remove   Removes the service
+        --run      Attempt to run the program as a service
+    `)
+}
