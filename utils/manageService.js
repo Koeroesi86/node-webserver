e1fa682c8eefdc33bcb545204a91acab114e7280
Krisztian Korosi <koeroesi86@gmail.com>
2018-06-23 21:05:00 +0100

    Added service process handling

--- utils/manageService.js
+++ utils/manageService.js
@@ -2,25 +2,39 @@ const service = require('os-service');
 const minimist = require('minimist');
 const {resolve} = require('path');
 const {SERVICE_NAME} = require('../configuration');
+const {spawn} = require('child_process');
 
 const {add, remove, run} = minimist(process.argv.slice(2));
 
+let child;
+
 if (add) {
-    service.add(SERVICE_NAME, {programPath: resolve('../server.js')}, function(error){
+    service.add(SERVICE_NAME, {
+        programArgs: ["--run"]
+    }, function (error) {
         if (error) {
             console.trace(error);
         }
     });
 } else if (remove) {
-    service.remove(SERVICE_NAME, function(error){
+    service.remove(SERVICE_NAME, function (error) {
         if (error) {
             console.trace(error);
         }
     });
 } else if (run) {
     service.run(function () {
+        if (child) {
+            child.stdin.pause();
+            child.kill();
+            child = null;
+        }
         service.stop(0);
     });
+
+    child = spawn('node', [resolve('../server.js')]);
+
+
 } else {
     console.info(`
     Usage:
