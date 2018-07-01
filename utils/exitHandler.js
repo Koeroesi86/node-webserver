0bcc8e9f52274f2d1cee724ae082f0d8ce64f598
Krisztian Korosi <koeroesi86@gmail.com>
2018-07-01 12:26:57 +0100

    Refactored exit listener for logging

--- utils/exitHandler.js
+++ utils/exitHandler.js
@@ -8,24 +8,30 @@ function exitHandler(instances) {
     process.exit(0);
 }
 
+function exitListener(instances, reason, event) {
+    console.log(`${reason} triggered:\n`, event);
+
+    exitHandler(instances);
+}
+
 function addExitListeners(instances) {
     process.stdin.resume();
 
     // do something when app is closing
-    process.on('exit', () => exitHandler(instances));
+    process.on('exit', (e) => exitListener(instances, 'exit', e));
 
     // catches ctrl+c event
-    process.on('SIGINT', () => exitHandler(instances));
+    process.on('SIGINT', (e) => exitListener(instances, 'SIGINT', e));
 
-    process.on('SIGHUP', () => exitHandler(instances));
-    process.on('SIGTERM', () => exitHandler(instances));
+    process.on('SIGHUP', (e) => exitListener(instances, 'SIGHUP', e));
+    process.on('SIGTERM', (e) => exitListener(instances, 'SIGTERM', e));
 
     // catches "kill pid" (for example: nodemon restart)
-    process.on('SIGUSR1', () => exitHandler(instances));
-    process.on('SIGUSR2', () => exitHandler(instances));
+    process.on('SIGUSR1', (e) => exitListener(instances, 'SIGUSR1', e));
+    process.on('SIGUSR2', (e) => exitListener(instances, 'SIGUSR2', e));
 
     //catches uncaught exceptions
-    process.on('uncaughtException', () => exitHandler(instances));
+    process.on('uncaughtException', (e) => exitListener(instances, 'uncaughtException', e));
 
 }
 
