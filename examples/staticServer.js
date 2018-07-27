0ebf9d1e33b4be04216167c153885bec48c61425
Krisztian Korosi <koeroesi86@gmail.com>
2018-07-27 19:53:00 +0100

    Added https config option to static server

--- examples/staticServer.js
+++ examples/staticServer.js
@@ -1,11 +1,13 @@
 const minimist = require('minimist');
 const express = require('express');
-const {resolve} = require('path');
+const { resolve } = require('path');
+const https = require('https');
+const http = require('http');
 
-const {path, port} = minimist(process.argv.slice(2));
+const { path, port, secureConfig } = minimist(process.argv.slice(2));
 
-if(!port || !path) {
-    throw new Error("Both port and path should be specified to serve.");
+if (!port || !path) {
+  throw new Error("Both port and path should be specified to serve.");
 }
 
 const resolved = resolve(path);
@@ -17,7 +19,23 @@ staticApp.use(express.static(resolved));
 
 staticApp.disable('x-powered-by');
 
-staticApp.listen(port);
+const startHttp = () => http.createServer(staticApp).listen(port);
+const startHttps = (config) => https.createServer(config, staticApp).listen(port);
+
+if (!secureConfig) {
+  startHttp();
+} else {
+  const { key, cert } = JSON.parse(secureConfig);
+
+  if (key && cert) {
+    startHttps({
+      key: fs.readFileSync(resolve(key)),
+      cert: fs.readFileSync(resolve(cert)),
+    });
+  } else {
+    startHttp();
+  }
+}
 
 console.log(`Providing static files to "${resolved}" on port ${port}`);
 
