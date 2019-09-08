3e48f3b72e475032bbc7cf9a75201a3ea4e1f26a
Krisztian Korosi <koeroesi86@gmail.com>
2019-09-08 09:30:33 +0100

    Adds multiple log arguments

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,9 @@
 const chalk = require('chalk');
 
 module.exports = {
-  system: message => console.log(chalk.blue(message)),
-  info: message => console.log(chalk.magenta(message)),
-  success: message => console.log(chalk.green(message)),
-  error: message => console.log(chalk.red(message)),
-  warning: message => console.log(chalk.yellow(message)),
+  system: (...args) => console.log(chalk.blue(...args)),
+  info: (...args) => console.log(chalk.magenta(...args)),
+  success: (...args) => console.log(chalk.green(...args)),
+  error: (...args) => console.log(chalk.red(...args)),
+  warning: (...args) => console.log(chalk.yellow(...args)),
 };
