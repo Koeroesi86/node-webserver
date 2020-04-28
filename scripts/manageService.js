f897acac6a73255a7628e93b82d66237dcfc4c08
Krisztian Korosi <koeroesi86@gmail.com>
2020-04-28 19:50:10 +0100

    Cleaned manageService.js confusion

--- scripts/manageService.js
+++ scripts/manageService.js
@@ -1,54 +1,32 @@
 const service = require('os-service');
 const { resolve } = require('path');
 const { SERVICE_NAME } = require(process.env.NODE_WEBSERVER_CONFIG || '../configuration');
-const { spawn } = require('child_process');
 const parseArgv = require('../utils/parseArgv');
 
 const { add, remove, run } = parseArgv();
 
-let child;
 process.chdir(__dirname);
 
 if (add) {
   service.add(SERVICE_NAME, {
-    programPath: resolve('./manageService.js'),
-    programArgs: ["--run"]
-  }, function (error) {
+    programPath: resolve(__dirname, './server.js'),
+  }, error => {
     if (error) {
       console.trace(error);
     }
   });
 } else if (remove) {
-  service.remove(SERVICE_NAME, function (error) {
+  service.remove(SERVICE_NAME, error => {
     if (error) {
       console.trace(error);
     }
   });
 } else if (run) {
   service.run(() => {
-    if (child) {
-      child.stdin.pause();
-      child.kill();
-      child = null;
-    }
-    process.kill(process.pid, 'SIGINT');
     service.stop(0);
   });
-
-  child = spawn(
-    'node',
-    [ resolve(__dirname, './server.js') ],
-    { stdio: ['ignore', 'ignore', 'ignore', 'ipc'] }
-  );
-  process.on('exit', () => {
-    try {
-      child.kill();
-    } catch (e) {
-      //
-    }
-  });
 } else {
-  console.info(`
+  console.info('\x1b[32m%s\x1b[0m', `
     Usage:
     
     yarn run service [argument]
@@ -57,5 +35,5 @@ if (add) {
         --add      Installs the service
         --remove   Removes the service
         --run      Attempt to run the program as a service
-    `)
+    `);
 }
