13952a496fb947832ac3e282542f98e67d1251c7
Krisztian Korosi <koeroesi86@gmail.com>
2022-03-21 08:53:28 +0100

    Upgraded dependencies

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,3 @@
-const chalk = require('chalk');
 const moment = require('moment');
 const { resolve } = require('path');
 const { appendFileSync, mkdirSync, existsSync } = require('fs');
@@ -24,26 +23,26 @@ module.exports = {
   system: (...args) => {
     if (logLevels && logLevels.system === false) return;
     fileLog(resolve(fileLogPath, `./${startedAt.valueOf()}.system.log`), args);
-    return console.log(chalk.blue(...args));
+    return console.log(...args);
   },
   info: (...args) => {
     if (logLevels && logLevels.info === false) return;
     fileLog(resolve(fileLogPath, `./${startedAt.valueOf()}.info.log`), args);
-    return console.log(chalk.magenta(...args));
+    return console.log(...args);
   },
   success: (...args) => {
     if (logLevels && logLevels.success === false) return;
     fileLog(resolve(fileLogPath, `./${startedAt.valueOf()}.success.log`), args);
-    return console.log(chalk.green(...args));
+    return console.log(...args);
   },
   error: (...args) => {
     if (logLevels && logLevels.error === false) return;
     fileLog(resolve(fileLogPath, `./${startedAt.valueOf()}.error.log`), args);
-    return console.log(chalk.red(...args));
+    return console.log(...args);
   },
   warning: (...args) => {
     if (logLevels && logLevels.warning === false) return;
     fileLog(resolve(fileLogPath, `./${startedAt.valueOf()}.warning.log`), args);
-    return console.log(chalk.yellow(...args));
+    return console.log(...args);
   },
 };
